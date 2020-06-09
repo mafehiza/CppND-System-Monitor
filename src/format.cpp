16ba11a0639bc987d41f0f3a656786a725d008f2
mafehiza <mafehiza@gmail.com>
2020-06-09 18:57:43 +0200

    Get System Data

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,8 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+    int minutes = seconds / 60;
+    int hours = minutes / 60;
+    return std::to_string(hours) + ":" + std::to_string(minutes%60) + ":" + std::to_string(seconds%60);
+}
